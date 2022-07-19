ceb6bea0578523861b4824a14129d66f2d46e5db
FL4RE9N <vsr2154@gmail.com>
2022-07-19 14:23:41 +0530

    few minor changes

--- calc.java
+++ calc.java
@@ -2,45 +2,34 @@ import java.util.Scanner;
 
 public class calc {
     public static void main(String arg[]) {
-        String des = "YES";
+        String des = "yes";
         Scanner sc = new Scanner(System.in);
         do {
             System.out.println("choose a operation\n1.add\n2.sub\n3.multi\n4.div\n");
             String operation = sc.nextLine();
-            //ez run
+            System.out.println("enter numbers: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
             if (operation.equals("add")) {
-                System.out.println("enter numbers");
-                int a = sc.nextInt();
-                int b = sc.nextInt();
                 int ans = a + b;
                 System.out.println(ans);
             } else if (operation.equals("sub")) {
-                System.out.println("enter numbers");
-                int a = sc.nextInt();
-                int b = sc.nextInt();
                 int ans = a - b;
                 System.out.println(ans);
             } else if (operation.equals("multi")) {
-                System.out.println("enter numbers");
-                int a = sc.nextInt();
-                int b = sc.nextInt();
-                int ans = a * b;
+                int ans = a * b; 
                 System.out.println(ans);
             } else if (operation.equals("div")) {
-                System.out.println("enter numbers");
-                int a = sc.nextInt();
-                int b = sc.nextInt();
                 int ans = a / b;
                 System.out.println(ans);
             } else {
                 System.out.println("invalid");
             }
-            System.out.println("do u want to continue");
+            System.out.println("do u want to continue ?");
             des = sc.nextLine();
             des = sc.nextLine();
             // run
-        } while (des.equals("YES"));
-
+        } while (des.equals("yes"));
         sc.close();
     }
 }
